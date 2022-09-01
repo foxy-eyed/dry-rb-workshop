27df1d30b5ad6ce937028a5774f56fdd98aed483
Anna Selivanova <foxy.eyed@gmail.com>
2022-09-01 17:02:03 +0400

    Tmp: ensure CI check works

--- apps/in_memory/transport/testers_accounting_request.rb
+++ apps/in_memory/transport/testers_accounting_request.rb
@@ -4,6 +4,7 @@ module InMemory
   module Transport
     class TestersAccountingRequest
       include Import[service: "contexts.testers_accounting.service"]
+      include Import[illegal: "contexts.toy_testing.service"]
 
       def call
         puts "Handle testers accounting request..."
